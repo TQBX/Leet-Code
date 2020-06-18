453a5e09c4e610d11c8321ee1985ac3a50290e71
TQBX <hyh1332790762@163.com>
2020-06-18 21:12:14 +0800

    力扣币,

---
+++ src/array/Solution7.java
@@ -0,0 +1,58 @@
+package array;
+
+/**
+ * @author Summerday
+ * <p>
+ * 桌上有 n 堆力扣币，每堆的数量保存在数组 coins 中。我们每次可以选择任意一堆，拿走其中的一枚或者两枚，求拿完所有力扣币的最少次数。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[4,2,1]
+ * <p>
+ * 输出：4
+ * <p>
+ * 解释：第一堆力扣币最少需要拿 2 次，第二堆最少需要拿 1 次，第三堆最少需要拿 1 次，总共 4 次即可拿完。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：[2,3,10]
+ * <p>
+ * 输出：8
+ * <p>
+ * 限制：
+ * <p>
+ * 1 <= n <= 4
+ * 1 <= coins[i] <= 10
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/na-ying-bi
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution7 {
+
+    public int minCount(int[] coins) {
+        int temp = 0;
+        int sum = 0;
+        for (int coin : coins) {
+            temp = coin % 2 == 1 ? coin / 2 + 1 : coin / 2;
+            sum += temp;
+        }
+        return sum;
+    }
+
+    public int minCountEasy(int[] coins){
+        int sum = 0;
+        for (int coin : coins) {
+            sum+=coin+1>>1;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+        int i = s.minCountEasy(new int[]{2, 3, 10});
+        int i2 = s.minCountEasy(new int[]{4,2,1});
+        System.out.println(i);
+        System.out.println(i2);
+    }
+}

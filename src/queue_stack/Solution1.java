19ad0044d91736757f281e6cfe17eebc334a34c6
TQBX <hyh1332790762@163.com>
2020-06-19 23:19:09 +0800

    两个栈实现队列

---
+++ src/queue_stack/Solution1.java
@@ -0,0 +1,54 @@
+package queue_stack;
+
+import java.util.LinkedList;
+
+/**
+ * @author Summerday\
+ *
+ * 剑指 Offer 09. 用两个栈实现队列
+ *
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution1 {
+
+
+    public static void main(String[] args) {
+        CQueue cQueue = new CQueue();
+        cQueue.appendTail(3);
+        cQueue.appendTail(4);
+        System.out.println(cQueue.deleteHead());
+        System.out.println(cQueue.deleteHead());
+
+
+    }
+
+}
+class CQueue {
+
+    private final LinkedList<Integer> stack1;
+    private final LinkedList<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.add(value);
+    }
+
+    public int deleteHead() {
+        if(stack2.isEmpty()){
+            if (stack1.isEmpty()) return -1;
+            while (!stack1.isEmpty()) {
+                stack2.add(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+}

2549d5f9cfba61da37d4c1c4f1778c6fc8b3cebe
TQBX <hyh1332790762@163.com>
2020-06-29 22:27:59 +0800

    树的节点

---
+++ src/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package tree;
+
+/**
+ * 树节点
+ * created by Summer-day
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}

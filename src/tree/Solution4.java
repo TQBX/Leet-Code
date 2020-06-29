455572ad0426d982823280e1accbfe1d768798e8
TQBX <hyh1332790762@163.com>
2020-06-30 01:23:18 +0800

    增加了 层序遍历

---
+++ src/tree/Solution4.java
@@ -0,0 +1,79 @@
+package tree;
+
+import java.util.*;
+
+/**
+ * created by Summer-day
+ * <p>
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * <p>
+ * Root
+ * / \
+ * l   r
+ * / \ / \
+ */
+public class Solution4 {
+
+    /**
+     * 请实现一个函数，用来判断一棵二叉树是不是对称的
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return recur(root.left, root.right);
+    }
+
+    private boolean recur(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.val != right.val) return false;
+        return recur(left.left, right.right) && recur(left.right, right.left);
+
+    }
+
+    /**
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+
+    /**
+     * 层序遍历
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> oneLevel = new ArrayList();
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                oneLevel.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.addFirst(oneLevel);
+        }
+        return res;
+    }
+}

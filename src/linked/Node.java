39769d5720fd5727b6f234c086c87346553c8687
TQBX <hyh1332790762@163.com>
2020-07-07 14:24:48 +0800

    增加Node节点

---
+++ src/linked/Node.java
@@ -0,0 +1,17 @@
+package linked;
+
+/**
+ * Node对象定义
+ * created by Summer-day
+ */
+public class Node {
+	public int val;
+	public Node next;
+	public Node random;
+	
+	public Node(int val) {
+		this.val = val;
+		this.next = null;
+		this.random = null;
+	}
+}
